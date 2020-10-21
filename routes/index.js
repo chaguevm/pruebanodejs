7987c770a299dd1193a8a54c114df5c804c7023b
Darwin Valero <devm_4648@hotmail.com>
2020-10-21 13:28:14 -0500

    120 mins

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,21 @@ const router = express.Router();
 const pool = require('../database');
 const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
 
-router.get('/', isLoggedIn, (req, res) => {
-    res.render('tweets.hbs');
+router.get('/', isLoggedIn, async (req, res) => {
+    const query = `SELECT * FROM contacts WHERE user_id = ${req.user.id}`;
+    const contacts = await pool.query(query);
+    res.render('home.hbs', {contacts});
 });
 
+router.get('/newcontact', isLoggedIn, (req, res) =>{
+    res.render('newcontact.hbs');
+});
+
+router.post('/newcontact', isLoggedIn, async (req, res) => {
+    const query = `INSERT INTO contacts (user_id, first_name, last_name, email, number)
+        VALUES (${req.user.id}, '${req.body.first_name}', '${req.body.last_name}','${req.body.email}','${req.body.number}')`;
+    const newcontact = await pool.query(query);
+    req.flash('success','Contact added')
+    res.redirect('/');
+})
 module.exports = router;
