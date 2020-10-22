a9baa31849f9c116a5eb10b1bfaf1e3357d41d80
Darwin Valero <devm_4648@hotmail.com>
2020-10-21 20:08:00 -0500

    fix keys

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     database: {
         host: process.env.DB_HOST || 'localhost',
-        user: process.env.FB_USER || 'root',
+        user: process.env.DB_USER || 'root',
         password: process.env.DB_PASS || '123456',
         database: process.env.DB_NAME || 'contactlist',
         charset : 'utf8'
